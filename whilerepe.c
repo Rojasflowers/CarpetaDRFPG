8b83a7025caffefee28e9a980210bed843f476e4
flores <daniel.rojas.cb3@gmail.com>
2024-05-03 11:09:20 -0600

    agrege más código

--- whilerepe.c
+++ whilerepe.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int n;
+	int n,co,a;
+    co=0;
     do{
 	printf("ingrese un número\n");
 	scanf("%i",&n);
-	printf("has introducido este número:\n %i\n",n);}
+	printf("has introducido este número:\n %i\n",n);
+    co=co+1;
+    a=a+n;}
 	while (n!=0);{
 		printf("finalizado:se ha introducido el número 0\n");}
+        printf("se contaron:%i\n",co);
+        printf("se acumularon:%i\n",a);
 	return 0;
 }
